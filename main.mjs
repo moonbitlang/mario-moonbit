dfd9b9e5055fbd36d3e5b39ac807c05afde04129
zihang <yezihang@idea.edu.cn>
2024-05-09 11:32:55 +0800

    refactor: move anime logic

--- main.mjs
+++ main.mjs
@@ -7,13 +7,6 @@ const HEIGHT = 256;
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
 
-let requestAnimationFrameId = null;
-let lastTime = 0;
-let dropCounter = 0;
-let dropInterval = 10;
-
-let game_update = null;
-
 let memory
 
 const importObject = {
@@ -25,27 +18,11 @@ const importObject = {
     }
 };
 
-function update(time = 0) {
-    const deltaTime = time - lastTime;
-    dropCounter += deltaTime;
-    if (dropCounter > dropInterval) {
-        game_update(context, time);
-        dropCounter = 0;
-    }
-    lastTime = time;
-    requestAnimationFrameId = requestAnimationFrame(update);
-}
-
-function start() {
-    WebAssembly.instantiateStreaming(
-        fetch("target/wasm-gc/release/build/main/main.wasm"),
-        importObject
-    ).then((obj) => {
-        memory = obj.instance.exports["moonbit.memory"];
-        obj.instance.exports._start();
-        obj.instance.exports.start(context);
-        game_update = obj.instance.exports["game_update"];
-        requestAnimationFrameId = requestAnimationFrame(update);
-    });
-}
-start();
+WebAssembly.instantiateStreaming(
+    fetch("target/wasm-gc/release/build/main/main.wasm"),
+    importObject
+).then((obj) => {
+    memory = obj.instance.exports["moonbit.memory"];
+    obj.instance.exports._start();
+    obj.instance.exports.entry(context);
+});
